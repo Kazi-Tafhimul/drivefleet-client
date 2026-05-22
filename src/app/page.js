1d900095218e390ea45bb9919451470de959e700
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-05-22 19:33:57 +0600

    home page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
+import { FeaturesSection } from "@/components/FeatureSection";
 import Hero from "@/components/Hero";
+import { HowItWorksSection } from "@/components/HowItWorks";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Hero></Hero>
+      <FeaturesSection></FeaturesSection>
+      <HowItWorksSection></HowItWorksSection>
     </div>
     
   );
